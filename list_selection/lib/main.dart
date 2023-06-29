8872d086e45b58816eb111a46af0ae722fb046f2
GabbyGreat <gabrieloranekwu@gmail.com>
2023-06-29 23:46:00 +0100

    :fire: Made an even better UI

--- list_selection/lib/main.dart
+++ list_selection/lib/main.dart
@@ -16,15 +16,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -38,7 +36,14 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('List selection'),
+        actions: [
+          if (enteredMulti)
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+            )
+        ],
       ),
       body: ListView.builder(
         itemCount: 20,
@@ -61,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: ListTile(
             leading: (enteredMulti && selectedList.contains(index))
                 ? const Icon(Icons.check_circle)
-                : null,
+                : const Icon(Icons.circle),
             title: Text('Tile $index'),
           ),
         ),
